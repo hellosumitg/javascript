699073c36396c43f8f4fe9ee0c5e255d7e9e7a01
hellosumitg <hellosumitg.com>
2023-12-03 23:06:36 +0530

    strict mode

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -44,3 +44,20 @@
 */
 
 
+// Strict Mode in Javascript:
+'use strict';
+
+const personName = 'Candy'; // without declaring also browser understand that it will be a variable as there is no strict mode
+// but if implemented it will give error
+/*
+    Uncaught ReferenceError: personName is not defined
+        at app.js:50:12
+    (anonymous) @ app.js:50
+*/
+
+var undefined = 5; // now this will throw error but if there is no `strict mode` `var` will not throw error
+// if we use predefined keywords as variable name
+
+
+console.log(personName);
+

fb77d097923e81eb5fc1c1c6e93301fe0d9a9e6c
hellosumitg <hellosumitg@yahoo.com>
2023-12-14 10:34:43 +0530

    `.filter()` method

--- arrays-01/app.js
+++ arrays-01/app.js
@@ -207,3 +207,10 @@ const sortedPrices = prices.sort((a, b) => {
 });
 console.log('due to our logic gives reverse order', sortedPrices); 
 console.log("due to our logic .reverse() gives sorted order", sortedPrices.reverse());
+
+
+// filtering arrays with `.filter()` method which filters and returns a new array
+const filteredArray = prices.filter((price, idx, prices) => {
+  return price > 6
+});
+console.log(filteredArray);

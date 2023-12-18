b113e0325f541d764d6a3711c2c04507bc598a3c
hellosumitg <hellosumitg@yahoo.com>
2023-12-18 09:44:55 +0530

    Property Types & Property Order

--- objects-01/assets/scripts/objects.js
+++ objects-01/assets/scripts/objects.js
@@ -6,7 +6,10 @@ let person = {
   hobbies: ['Sports', 'Cooking'], // property
   greet: function() { // method
     alert('Hi there!');
-  }
+  }, 
+  // -1.5: 'hello', // -ve numbers are not allowed as `key`
+  1.5: 'hello' // here `key` is a `number` but in the background it will be coerced to a `string`
+  // this way is only exclusive to `objects`
 };  
 
 // Adding, Modifying & Deleting Properties
@@ -25,12 +28,24 @@ console.log(person);
 
 // Special Key Names & Square Bracket Property Access:-
 console.log('First Name: ', person['first name']); // In this case for retrieving or accessing any `key(i.e property of an object)` 
-// we will use the same way we did in the case of `arrays` as both are `objects`.
-
-// as 
+// we will use the same way we did in the case of `arrays` as both are `objects` as the `person.someKey`(i.e Dot notation will not work here)
+// as we had learned in DOM section
 const movieList = document.getElementById('movie-list');
 // movieList.style.backgroundColor = 'red'; // as we had learned earlier, retrieving or accessing `key's(i.e 'backgroundColor')` value of `style` object
 // but it can also work the same way if written like the below way
 // movieList.style['backgroundColor'] = 'red'; // this way is also write for retrieving or accessing `key's(i.e 'backgroundColor')` value of `style` object
 movieList.style['background-color'] = 'red'; // writing `key` in the form of `css` way is also right for retrieving or accessing `key's(i.e 'background-color')` value of `style` object
 movieList.style.display = 'block';
+
+
+// Property Types & Property Order:-
+// below the `Dot notation will not work like `person.someKeyName` for retrieving or accessing it's value
+// two ways
+console.log("1.5's value: ", person["1.5"]); // at the end all `keys` are coerced as `string`
+console.log("1.5's value: ", person[1.5]);
+
+console.log(person); // ordered in the same way we have defined or inserted in the object
+
+// but in the case of `numbers` as `key` it will be sorted in ascending order like in case of `array` which is also an `object`
+const numbers = {5: 'hi', 1: 'true'};
+console.log(numbers); // we get it in ascending order
